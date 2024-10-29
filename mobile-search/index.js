4ed315fe9f9461663db64ed94ae0907048017aad
shivam.sharma21 <shivam.sharma2@velsof.com>
2024-10-29 15:43:07 +0530

    Added the mobile search bar with typing animation in it

---
+++ mobile-search/index.js
@@ -0,0 +1,59 @@
+addEventListener("load", () => {
+  const input = document.querySelector(".mobile-header-search-field");
+  const placeholders = [
+    "Search for Club",
+    "Search for Country",
+    "Search for Player",
+    "Search for Product",
+  ];
+  let currentPlaceholderIndex = 0;
+  let currentCharIndex = 0;
+  let typingDelay = 150;
+  let erasingDelay = 100;
+  let newPlaceholderDelay = 2000;
+  let cursorVisible = true;
+
+  function toggleCursor() {
+    input.placeholder = input.placeholder.endsWith("|")
+      ? input.placeholder.slice(0, -1)
+      : input.placeholder + "|";
+  }
+
+  function typePlaceholder() {
+    if (currentCharIndex < placeholders[currentPlaceholderIndex].length) {
+      input.placeholder =
+        placeholders[currentPlaceholderIndex].substring(
+          0,
+          currentCharIndex + 1
+        ) + "|";
+      currentCharIndex++;
+      setTimeout(typePlaceholder, typingDelay);
+    } else {
+      cursorVisible = false;
+      setTimeout(() => {
+        cursorVisible = true;
+        setInterval(toggleCursor, 500); // Start blinking cursor after typing
+      }, newPlaceholderDelay);
+      setTimeout(erasePlaceholder, newPlaceholderDelay);
+    }
+  }
+
+  function erasePlaceholder() {
+    if (currentCharIndex > 0) {
+      input.placeholder =
+        placeholders[currentPlaceholderIndex].substring(
+          0,
+          currentCharIndex - 1
+        ) + "|";
+      currentCharIndex--;
+      setTimeout(erasePlaceholder, erasingDelay);
+    } else {
+      currentPlaceholderIndex =
+        (currentPlaceholderIndex + 1) % placeholders.length;
+      setTimeout(typePlaceholder, typingDelay);
+    }
+  }
+
+  setInterval(toggleCursor, 500); // Initial cursor blink
+  setTimeout(typePlaceholder, newPlaceholderDelay);
+});
