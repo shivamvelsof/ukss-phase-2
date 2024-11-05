b3e2ecbd59856b20bb88f967f31f5796a357a74c
shivam.sharma21 <shivam.sharma2@velsof.com>
2024-11-05 12:11:22 +0530

    Added the changes in the mobile search UI

--- mobile-search/index.js
+++ mobile-search/index.js
@@ -1,8 +1,10 @@
 addEventListener("DOMContentLoaded", () => {
   const input = document.querySelector(".mobile-header-search-field");
-  const placeholders = [
-    "Search for Club, Country, Player or Product",
-  ];
+  const Desktopinput = document.querySelector(".textSearch_1");
+  const desktopSearchPopup = document.querySelector(
+    ".search-bar .search-input"
+  );
+  const placeholders = ["Search for Club, Country, Player or Product"];
   let currentPlaceholderIndex = 0;
   let currentCharIndex = 0;
   let typingDelay = 50;
@@ -14,11 +16,29 @@ addEventListener("DOMContentLoaded", () => {
     input.placeholder = input.placeholder.endsWith("|")
       ? input.placeholder.slice(0, -1)
       : input.placeholder + "|";
+    Desktopinput.placeholder = Desktopinput.placeholder.endsWith("|")
+      ? Desktopinput.placeholder.slice(0, -1)
+      : Desktopinput.placeholder + "|";
+    desktopSearchPopup.placeholder = desktopSearchPopup.placeholder.endsWith(
+      "|"
+    )
+      ? desktopSearchPopup.placeholder.slice(0, -1)
+      : desktopSearchPopup.placeholder + "|";
   }
 
   function typePlaceholder() {
     if (currentCharIndex < placeholders[currentPlaceholderIndex].length) {
       input.placeholder =
+        placeholders[currentPlaceholderIndex].substring(
+          0,
+          currentCharIndex + 1
+        ) + "|";
+      Desktopinput.placeholder =
+        placeholders[currentPlaceholderIndex].substring(
+          0,
+          currentCharIndex + 1
+        ) + "|";
+      desktopSearchPopup.placeholder =
         placeholders[currentPlaceholderIndex].substring(
           0,
           currentCharIndex + 1
@@ -31,13 +51,23 @@ addEventListener("DOMContentLoaded", () => {
         cursorVisible = true;
         setInterval(toggleCursor, 500); // Start blinking cursor after typing
       }, newPlaceholderDelay);
-    //   setTimeout(erasePlaceholder, newPlaceholderDelay);
+      setTimeout(erasePlaceholder, newPlaceholderDelay);
     }
   }
 
   function erasePlaceholder() {
     if (currentCharIndex > 0) {
       input.placeholder =
+        placeholders[currentPlaceholderIndex].substring(
+          0,
+          currentCharIndex - 1
+        ) + "|";
+      Desktopinput.placeholder =
+        placeholders[currentPlaceholderIndex].substring(
+          0,
+          currentCharIndex - 1
+        ) + "|";
+      desktopSearchPopup.placeholder =
         placeholders[currentPlaceholderIndex].substring(
           0,
           currentCharIndex - 1
