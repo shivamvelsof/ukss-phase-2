5068c540d33eae3d8125d929d4cef98c21e3b570
shivam.sharma21 <shivam.sharma2@velsof.com>
2024-11-11 12:22:49 +0530

    Completed the upselling product changes

--- sidebar-cart-v2-2/index.js
+++ sidebar-cart-v2-2/index.js
@@ -16,9 +16,74 @@ $(document).ready(function () {
     });
 
   $(".add-sc-btn").click(function () {
-    $(this)
-      .parents(".suggestion-card")
-      .find(".sc-select-size-wrapper")
-      .addClass("show-size-dd");
+    const select = $(this)
+      .parents(".sc-action-wrapper")
+      .find(".sc-select-size");
+    if (select.val() === "") {
+      $(select).parents(".sc-select-size-content").addClass("select-error");
+    }
   });
+
+  $(".sc-select-size").change(function () {
+    const value = $(this).val();
+    $(this).parents(".sc-select-size-content").removeClass("select-error");
+    if (value === "") {
+      $(this).parents(".suggestion-card").removeClass("ready-to-add");
+    } else {
+      $(this).parents(".suggestion-card").addClass("ready-to-add");
+    }
+  });
+
+  // let shakeInterval;
+
+  // function startAnimation() {
+  //   clearInterval(shakeInterval);
+  //   $(".suggestion-card").removeClass("shake-card");
+  //   shakeInterval = setInterval(() => {
+  //     $(".suggestion-card").addClass("shake-card");
+  //     setTimeout(() => {
+  //       $(".suggestion-card").removeClass("shake-card");
+  //     }, 700);
+  //   }, 3500);
+  // }
+
+  // function stopAnimation() {
+  //   clearInterval(shakeInterval);
+  //   $(".suggestion-card").removeClass("shake-card");
+  // }
+
+  // $(".suggestion-card").hover(
+  //   function () {
+  //     // over
+  //     stopAnimation();
+  //   },
+  //   function () {
+  //     // out
+  //     startAnimation();
+  //   }
+  // );
+  // // Start the animation initially
+  // startAnimation();
+
+  // function animateSuggestionCard() {
+  //   setInterval(() => {
+  //     $(".suggestion-card").addClass("shake-card");
+  //     setTimeout(() => {
+  //       $(".suggestion-card").removeClass("shake-card");
+  //     }, 500); // Remove the class after 1 second
+  //   }, 5000); // Repeat the entire sequence every 5 seconds
+  // }
+
+  // animateSuggestionCard();
+
+  // function animateSuggestionCard() {
+  //   setInterval(() => {
+  //     $(".suggestion-card").addClass("shake-card");
+  //     setInterval(() => {
+  //       $(".suggestion-card").removeClass("shake-card");
+  //     }, 1000);
+  //   }, 5000);
+  // }
+
+  // animateSuggestionCard();
 });
