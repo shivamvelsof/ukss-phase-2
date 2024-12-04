3ec55f77d8c53deadbfcea23ce0c5d47f83f54d4
shivam.sharma21 <shivam.sharma2@velsof.com>
2024-12-04 12:38:51 +0530

    Completed the italian serie a page

--- italian/index.js
+++ italian/index.js
@@ -1,33 +1,10 @@
-new Swiper(".circular-cat-swiper", {
-  freeMode: !0,
-  scrollbar: {
-    el: ".circular-cat-swiper-scrollbar",
-    draggable: !0,
-  },
-  mousewheel: {
-    invert: !1,
-    forceToAxis: !0,
-    releaseOnEdges: !0,
-  },
-  breakpoints: {
-    0: {
-      slidesPerView: 2.5,
-    },
-    320: {
-      slidesPerView: 4.2,
-    },
-    400: {
-      slidesPerView: 4.2,
-    },
-    565: {
-      slidesPerView: 5.5,
-    },
-    767: {
-      slidesPerView: 7.5,
-    },
-    992: {
-      slidesPerView: 10.5,
-      spaceBetween: 10,
-    },
-  },
+$(document).ready(function () {
+  $('[data-toggle="league-modal"]').click(function () {
+    $('.league-modal-overlay').addClass('active-overlay');
+    $('.leagues-modal').addClass('active-modal');
+  });
+  $('.league-modal-overlay,.leagues-modal-close').click(function () {
+    $('.league-modal-overlay').removeClass('active-overlay');
+    $('.leagues-modal').removeClass('active-modal');
+  });
 });
